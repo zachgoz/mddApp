2306a0d2eaa9a3241ab8b827c0016c1898550749
zachgoz <zacharygosling@gmail.com>
2015-07-24 08:18:49 -0400

    Hard coded equipment dashboard numbers

--- routes/request-detail.js
+++ routes/request-detail.js
@@ -1,16 +1,18 @@
 var mongoose = require('mongoose');
 var Equipment = mongoose.model('Equipment');
+var ServiceOrder = mongoose.model('ServiceOrder');
 
 exports.deets = function(req, res){
-    Equipment.find({"SerialNumber": 'XR99960703'})
+    ServiceOrder.find({"_id":411339})
              .populate("_Equipment")
              .populate("_Product")
              .exec(function (err, equip){
-             		//console.log(equip);
+             		console.log(equip);
                 	res.render('request-detail', 
     				{ 
     					title: 'Details',
-    					data: equip
+    					data: equip,
+                        ServiceDetails: equip.ServiceDetails
 					});
               });
 };
